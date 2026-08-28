4e54502ba2fb94ced086f184ff82c61cbbcda127
sanjay kumar <devarasettysanjay9229@gmail.com>
2026-08-28 14:36:00 +0530

    adding changes

--- src/app/razor-payment-service.service.ts
+++ src/app/razor-payment-service.service.ts
@@ -7,8 +7,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class RazorPaymentServiceService {
 
-  private baseUrl ='http://localhost:8080/razor-pay'
-   // 'https://razor-pay-payment-application.onrender.com/razor-pay';
+  private baseUrl = 'https://razor-pay-payment-application.onrender.com/razor-pay';
 
 
   constructor(private http: HttpClient) {
