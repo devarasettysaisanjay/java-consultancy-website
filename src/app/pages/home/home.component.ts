3a5f584009aafaf6eb108127694201a760efdb36
sanjay kumar <devarasettysanjay9229@gmail.com>
2026-09-03 18:57:32 +0530

    changes added for google serach indexing

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 import { SERVICES } from 'src/app/data/services';
 
 
@@ -9,4 +10,22 @@ import { SERVICES } from 'src/app/data/services';
 })
 export class HomeComponent {
  services = SERVICES;
+
+  constructor(
+    private title: Title,
+    private meta: Meta
+  ) {}
+
+   ngOnInit(): void {
+
+    this.title.setTitle(
+      'JavaBridge Connect | Java Training & Full Stack Courses'
+    );
+
+    this.meta.updateTag({
+      name: 'description',
+      content:
+        'JavaBridge Connect provides practical Java training, Java Full Stack courses, Spring Boot, Microservices, AWS, Angular and SQL training with real-time IT projects and hands-on learning.'
+    });
+  }
 }
